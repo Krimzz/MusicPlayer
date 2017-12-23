dd06d7f9a02d87019238e52be07e83c20ea80f90
Andrei Budisan <andrei.budisan@gmail.com>
2017-12-23 20:15:30 +0200

    GUI done ... maybe

--- Music/src/application/Main.java
+++ Music/src/application/Main.java
@@ -1,25 +1,56 @@
 package application;
 
-import java.io.IOException;
-
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 
 public class Main extends Application {
+	public static Main current;
+	private double xOffset = 0;
+    private double yOffset = 0;
+
 	@Override
 	public void start(Stage stage) {
+		current = this;
+	    
 		try {
+			
 			Parent root = FXMLLoader.load(getClass().getResource("/application/Main.fxml"));
+			
+			//Movable window
+			
+            root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+           	public void handle(MouseEvent event) {
+               xOffset = event.getSceneX();
+               yOffset = event.getSceneY();
+            }
+            });
+       
+            root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+               stage.setX(event.getScreenX() - xOffset);
+               stage.setY(event.getScreenY() - yOffset);
+            }
+            });
+			
 			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.setFill(Color.TRANSPARENT);
 			stage.setScene(scene);
+			stage.initStyle(StageStyle.TRANSPARENT);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setOnCloseRequest(e -> Platform.exit());
 			stage.setTitle("Music Player App");
 			stage.show();
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
--- Music/src/application/MainController.java
+++ Music/src/application/MainController.java
@@ -1,7 +1,121 @@
 package application;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXSlider;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class MainController {
+	@FXML 
+	private AnchorPane paneMusic;
+	
+	@FXML
+    private AnchorPane pane_playlistMenu;
+	
+	@FXML 
+	private JFXButton playButton;
+	
+	@FXML 
+	private JFXButton nextButton;
+	
+	@FXML 
+	private JFXButton prevButton;
+	
+	@FXML 
+	private JFXButton addButton;
+	
+	@FXML 
+	private JFXButton removeButton;
+	
+	@FXML 
+	private JFXButton importButton;
+	
+	@FXML 
+	private JFXButton exportButton;
+	
+	@FXML 
+	private JFXButton btn_minimize;
+
+	@FXML
+	private JFXButton playlistMenu;
+
+    @FXML
+    private JFXButton btn_exit;
+    
+    @FXML
+    private JFXButton btn_exit_playlist;
+    
+    @FXML 
+	private JFXSlider slider;
+    
+    @FXML    
+    private Label nowPlaying;
+    
+    @FXML    
+    private Label music_now;
+    
+    @FXML    
+    private Label music_time;
+   
+    @FXML
+    private void handleButtonAction(MouseEvent event) {
+    	if(event.getSource() == btn_exit) {
+    		System.exit(0);
+    	}
+    	else
+    		if(event.getSource() == playlistMenu && pane_playlistMenu.isVisible() == false) {
+        		pane_playlistMenu.setVisible(true);
+        	}
+    		else 
+    			if(event.getSource() == playlistMenu && pane_playlistMenu.isVisible() == true)
+    				pane_playlistMenu.setVisible(false);
+    	if(event.getSource() == btn_exit_playlist) {
+    		pane_playlistMenu.setVisible(false);
+    	}
+    	if(event.getSource() == btn_minimize) {
+    		Stage stage = (Stage)((JFXButton)event.getSource()).getScene().getWindow();
+    		stage.setIconified(true);
+    	}
+    	if(event.getSource() == playButton)
+    	{
+    		
+    	}
+    	if(event.getSource() == nextButton)
+    	{
+    		
+    	}
+    	if(event.getSource() == prevButton)
+    	{
+    		
+    	}
+    	if(event.getSource() == addButton)
+    	{
+    		
+    	}
+    	if(event.getSource() == removeButton)
+    	{
+    		
+    	}
+    	if(event.getSource() == importButton)
+    	{
+    		
+    	}
+    	if(event.getSource() == exportButton)
+    	{
+    		
+    	}
+    	if(event.getSource() == slider)
+    	{
+    		
+    	}
+    }
+    //change lable name when music changes
+    /*private void chageMusic() {
+    	nowPlaying.setText((new Date()).toString());
+    }*/
 
 }
