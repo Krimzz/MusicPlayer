5576797e8dd91f7b1b256a671b27388024d51e54
Andrei Budisan <andrei.budisan@gmail.com>
2017-12-27 18:45:46 +0200

    gvc

--- Music/src/application/MainController.java
+++ Music/src/application/MainController.java
@@ -10,112 +10,100 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class MainController {
-	@FXML 
-	private AnchorPane paneMusic;
-	
 	@FXML
-    private AnchorPane pane_playlistMenu;
-	
-	@FXML 
+	private AnchorPane paneMusic;
+
+	@FXML
+	private AnchorPane pane_playlistMenu;
+
+	@FXML
 	private JFXButton playButton;
-	
-	@FXML 
+
+	@FXML
 	private JFXButton nextButton;
-	
-	@FXML 
+
+	@FXML
 	private JFXButton prevButton;
-	
-	@FXML 
+
+	@FXML
 	private JFXButton addButton;
-	
-	@FXML 
+
+	@FXML
 	private JFXButton removeButton;
-	
-	@FXML 
+
+	@FXML
 	private JFXButton importButton;
-	
-	@FXML 
+
+	@FXML
 	private JFXButton exportButton;
-	
-	@FXML 
+
+	@FXML
 	private JFXButton btn_minimize;
 
 	@FXML
 	private JFXButton playlistMenu;
 
-    @FXML
-    private JFXButton btn_exit;
-    
-    @FXML
-    private JFXButton btn_exit_playlist;
-    
-    @FXML 
+	@FXML
+	private JFXButton btn_exit;
+
+	@FXML
+	private JFXButton btn_exit_playlist;
+
+	@FXML
 	private JFXSlider slider;
-    
-    @FXML    
-    private Label nowPlaying;
-    
-    @FXML    
-    private Label music_now;
-    
-    @FXML    
-    private Label music_time;
-   
-    @FXML
-    private void handleButtonAction(MouseEvent event) {
-    	if(event.getSource() == btn_exit) {
-    		System.exit(0);
-    	}
-    	else
-    		if(event.getSource() == playlistMenu && pane_playlistMenu.isVisible() == false) {
-        		pane_playlistMenu.setVisible(true);
-        	}
-    		else 
-    			if(event.getSource() == playlistMenu && pane_playlistMenu.isVisible() == true)
-    				pane_playlistMenu.setVisible(false);
-    	if(event.getSource() == btn_exit_playlist) {
-    		pane_playlistMenu.setVisible(false);
-    	}
-    	if(event.getSource() == btn_minimize) {
-    		Stage stage = (Stage)((JFXButton)event.getSource()).getScene().getWindow();
-    		stage.setIconified(true);
-    	}
-    	if(event.getSource() == playButton)
-    	{
-    		
-    	}
-    	if(event.getSource() == nextButton)
-    	{
-    		
-    	}
-    	if(event.getSource() == prevButton)
-    	{
-    		
-    	}
-    	if(event.getSource() == addButton)
-    	{
-    		
-    	}
-    	if(event.getSource() == removeButton)
-    	{
-    		
-    	}
-    	if(event.getSource() == importButton)
-    	{
-    		
-    	}
-    	if(event.getSource() == exportButton)
-    	{
-    		
-    	}
-    	if(event.getSource() == slider)
-    	{
-    		
-    	}
-    }
-    //change lable name when music changes
-    /*private void chageMusic() {
-    	nowPlaying.setText((new Date()).toString());
-    }*/
+
+	@FXML
+	private Label nowPlaying;
+
+	@FXML
+	private Label music_now;
+
+	@FXML
+	private Label music_time;
+
+	@FXML
+	private void handleButtonAction(MouseEvent event) {
+		if (event.getSource() == btn_exit) {
+			System.exit(0);
+		} else if (event.getSource() == playlistMenu && pane_playlistMenu.isVisible() == false) {
+			pane_playlistMenu.setVisible(true);
+		} else if (event.getSource() == playlistMenu && pane_playlistMenu.isVisible() == true)
+			pane_playlistMenu.setVisible(false);
+		if (event.getSource() == btn_exit_playlist) {
+			pane_playlistMenu.setVisible(false);
+		}
+		if (event.getSource() == btn_minimize) {
+			Stage stage = (Stage) ((JFXButton) event.getSource()).getScene().getWindow();
+			stage.setIconified(true);
+		}
+		if (event.getSource() == playButton) {
+
+		}
+		if (event.getSource() == nextButton) {
+
+		}
+		if (event.getSource() == prevButton) {
+
+		}
+		if (event.getSource() == addButton) {
+
+		}
+		if (event.getSource() == removeButton) {
+
+		}
+		if (event.getSource() == importButton) {
+
+		}
+		if (event.getSource() == exportButton) {
+
+		}
+		if (event.getSource() == slider) {
+
+		}
+	}
+	// change lable name when music changes
+	/*
+	 * private void chageMusic() { nowPlaying.setText((new Date()).toString()); }
+	 */
 
 }
